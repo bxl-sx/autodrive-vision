f99013da8e6f7b5f0a099ce5eb2c2b9fc9fc087c
baoxiaoling <xiaoling.bao@hongjingdrive.com>
2020-08-24 17:14:08 +0800

    Update vision_processor.cpp

--- vision_processor.cpp
+++ vision_processor.cpp
@@ -2,11 +2,12 @@
 // All rights reserved.
 //
 // Author: Xiaoling Bao
-// Update: 2020-08-20
+// Update: 2020-08-24
 #include "vision_processor.h"
+// #include "framework/ad_resource_manager.h"
 #include <log4cxx/logger.h>
 
-#include <opencv2/opencv.hpp>
+// #include <opencv2/opencv.hpp>
 
 namespace autodrive{
 namespace vision{
@@ -21,30 +22,40 @@ VisionProcessor::VisionProcessor() noexcept
 
 VisionProcessor::~VisionProcessor() {Destroy();}
 
-void VisionProcessor::Init(std::string const& config_file_path) noexcept(false) {
-  AdService::Init(config_file_path, );
-  SurroundView::Init(yml_file_dir);
+void VisionProcessor::Init(std::string const& config_file_path, std::string const& yml_file_dir) noexcept(false) {
+  AdService::Init(config_file_path);
+  
+  // Init surround view
+  SurroundView::GetInstance().Init(yml_file_dir);
+
   SetStatus(AdService::Status::INITIALIZED);
-  std::cout << "Status INITIALIZED" << sed::endl;
+  Vps_printf("Status INITIALIZED\n");
   LOG4CXX_INFO(GetLogger(), "INIT SUCCESS");
 }
 
-void VisionProcessor::Init_() {
-  SetStatus(AdService::Status::INITIALIZED);
-  std::cout << "Status INITIALIZED" << sed::endl;
-  LOG4CXX_INFO(GetLogger(), "INIT SUCCESS");
-}
+// void VisionProcessor::Init_(std::string const& yml_file_dir) {
+//   // Init surround view
+//   SurroundView::GetInstance().Init(yml_file_dir);
+//   SetStatus(AdService::Status::INITIALIZED);
+//   Vps_printf("Status INITIALIZED\n");
+//   LOG4CXX_INFO(GetLogger(), "INIT SUCCESS");
+// }
 
 void StartUp() noexcept(false) {
+  AdScheduler& scheduler{AdScheduler::GetInstance();
   if (GetStatus() == AdService::Status::INITIALIZED ||
       GetStatus() == AdService::Status::STOPPED) {
-    
+    auto now{std::chrono::system_clock::now()};
+    AdScheduler::Task task{std::bind(&VisionProcessor::image_task, this), now, 0,
+      resource_manager.GetPriority(CommonSettings::kCameraServiceTaskName),
+      resource_manager.GetCpuMask(CommonSettings::kCameraServiceTaskName)};
     //start surround view stitch
-    // SurroundView::GetInstance().stitch(std::bind(&VisionProcessor::read_image, ));
-    SurroundView::GetInstance().stitch();
+    // SurroundView::GetInstance().stitch();
+    // image_task();
+    scheduler.Schedule(std::move(task));
     }
 
-    std::cout << "Status RUNNING" << std::endl;
+    Vps_printf("Status RUNNING\n");
     LOG4CXX_DEBUG(GetLogger(), "Status RUNNING");
     SetStatus(AdService::Status::RUNNING);
 }
@@ -58,15 +69,33 @@ void VisionProcessor::GracefulExit() noexcept{
   graceful_exit_ = true;
 }
 
-void VisionProcessor::read_image(std::string const& image_file_dir, std::vector<SurroundView::Image>& cam_imgs) {
-  cv::Mat in_image, out_image;
-  in_image = cv2.imread(path, flags = 1);
-  if (in_image.empty()) {
-    std::cout << "Error! Input image cannot be read..." << std::endl;
+void VisionProcessor::image_task() {
+  cv::Mat in_Mat, out_Mat;
+  // SurroundVIew::Image dst_img;
+  std::vector<SurroundView::Image> cam_imgs(4);
+  for (i = 0; i < 4; i++) c{
+    in_Mat = cv::imread(cam_filepath[i]);
+    if (in_Mat.empty()) {
+      Vps_printf("Error! Input image cannot be read...\n");
   }
-}
-//**********************************
+    if (in_Mat.isContinuous()) {
+      cam_imgs[i].data_ptr = in_Mat.data;
+    }
+  }
+  // in_Mat = cv2.imread(path, flags = 1);
 
+  SurroundView::GetInstance().stitch(dst_img, cam_imgs);
+  // write image file
+  std::uint8_t* data_ptr_ = dst_img.data_ptr;
+  std::uint8_t** array = new std::unit8[dst_img.width][dst_img.height];
+  for (std::int32_t i = 0; i < dst_img.width) {
+    for (std::int32_t j = 0; j < dst_img.height) {
+      array[i][j] = dst_ptr[i * dst_img.height + j];
+    }
+  }
+  cv::Mat img(dst_img.width, dst_img.height, CV_8UC1, array);
+  delete array;
+  cv2.imwrite(dst_filepath, dst_img);
 }
 
 }
